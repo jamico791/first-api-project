6d38f84856d6638f1d7b514a1769a6ab238446cb
John Amico <johnamico12@yahoo.com>
2020-07-11 17:54:09 -0400

    Added label

--- src/components/PokeForm.js
+++ src/components/PokeForm.js
@@ -3,6 +3,9 @@ import React from 'react'
 function PokeForm(props) {
 	return (
 		<form onSubmit={props.onSubmit}>
+			<label style={{ display: 'block' }}>
+				Enter Pokemon name or Pokedex number:{' '}
+			</label>
 			<input type="text" onChange={props.onChange} />
 			<button type="submit">Submit</button>
 		</form>
