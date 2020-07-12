31f94878bfa672ef8db3e719dd47b586cdbc175f
John Amico <john@pop-os.localdomain>
2020-07-11 23:33:24 -0400

    Added loading image

--- src/App.js
+++ src/App.js
@@ -1,65 +1,57 @@
-import React from 'react'
-import PokeImage from './components/PokeImage'
-import PokeForm from './components/PokeForm'
+import React from "react";
+import PokeImage from "./components/PokeImage";
+import PokeForm from "./components/PokeForm";
 
 class App extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			images: {},
-			value: '',
-			loading: false,
-		}
-		this.handleChange = this.handleChange.bind(this)
-		this.handleSubmit = this.handleSubmit.bind(this)
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: {},
+      value: "",
+      loading: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-	handleChange(event) {
-		const copy = { ...this.state }
-		copy.value = event.target.value
-		this.setState(copy)
-	}
+  handleChange(event) {
+    const copy = { ...this.state };
+    copy.value = event.target.value;
+    this.setState(copy);
+  }
 
-	handleSubmit(event) {
-		const copy = { ...this.state }
-		copy.loading = true
-		this.setState(copy)
-		event.preventDefault()
-		fetch(
-			`https://pokeapi.co/api/v2/pokemon/${this.state.value.toLowerCase()}`
-		)
-			.then(response => response.json())
-			.then(data =>
-				this.setState({
-					images: data.sprites,
-					loading: false,
-				})
-			)
-	}
+  handleSubmit(event) {
+    const copy = { ...this.state };
+    copy.loading = true;
+    this.setState(copy);
+    event.preventDefault();
+    fetch(`https://pokeapi.co/api/v2/pokemon/${this.state.value.toLowerCase()}`)
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          images: data.sprites,
+          loading: false,
+        })
+      );
+  }
 
-	render() {
-		let imageComponents = []
-		for (const [key, value] of Object.entries(this.state.images)) {
-			if (
-				value !== null &&
-				(key === 'back_default' || key === 'front_default')
-			) {
-				imageComponents.push(<PokeImage key={key} imageURL={value} />)
-			}
-		}
+  render() {
+    let imageComponents = [];
+    for (const [key, value] of Object.entries(this.state.images)) {
+      if (key === "front_default") {
+        imageComponents.push(<PokeImage key={key} imageURL={value} />);
+      }
+    }
 
-		imageComponents.reverse()
+    imageComponents.reverse();
 
-		return (
-			<div>
-				<PokeForm
-					onChange={this.handleChange}
-					onSubmit={this.handleSubmit}
-				/>
-				{this.state.loading ? 'loading...' : imageComponents}
-			</div>
-		)
-	}
+    return (
+      <div>
+        <PokeForm onChange={this.handleChange} onSubmit={this.handleSubmit} />
+        {this.state.loading ? <img src="loading.gif" alt="loading..."/> : imageComponents}
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
